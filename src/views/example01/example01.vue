e51b38b8d0a070833de13266cf5ce32827433c2e
xx <1141237081@qq.com>
2021-03-14 11:14:16 +0800

    1

---
+++ src/views/example01/example01.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <button @click="change">change</button>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data: () => ({
+    title: "Welcome"
+  }),
+  methods: {
+    change() {
+      this.$data.title = "Xiang";
+    }
+  }
+});
+</script>
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
---
+++ src/views/sidebar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <h1>Vue示例</h1>
+    <ul>
+      <li><router-link to="/example01">welcome</router-link></li>
+      
+    </ul>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
